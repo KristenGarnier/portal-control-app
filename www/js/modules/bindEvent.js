1fa089683eef5c3409cca6ae9294d2eb575894bb
KristenGarnier <garnier.kristen@icloud.com>
2016-02-07 14:35:15 +0100

    Adding touch gesture for changing the view

--- www/js/modules/bindEvent.js
+++ www/js/modules/bindEvent.js
@@ -1,4 +1,9 @@
+var Hammer = require('hammerjs');
+
 module.exports = function(){
+    var touchEvents = new Hammer(document.body);
+    console.log(touchEvents);
+
     document.addEventListener('deviceready', this.onDeviceReady, false);
     document.querySelector('#switch').addEventListener('click', function () {
         this.changeView();
@@ -6,4 +11,16 @@ module.exports = function(){
 
     $('body').on('request-success', this.successfull);
     $('body').on('request-error', this.errorish);
+
+    touchEvents.on('swiperight', function(e){
+        if($('#h').length === 0){
+            this.changeView();
+        }
+    }.bind(this));
+    touchEvents.on('swipeleft', function(e){
+        if($('#h').length > 0){
+            this.changeView();
+        }
+    }.bind(this));
+
 };
