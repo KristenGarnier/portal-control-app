e8c91e77ee61b0e9a16fd43f4c0073c1d0a39f18
KristenGarnier <garnier.kristen@icloud.com>
2016-02-07 14:36:19 +0100

    Adding a spinner for notifying the action is taking care

--- www/js/modules/makeRequest.js
+++ www/js/modules/makeRequest.js
@@ -1,10 +1,12 @@
 var request = require('superagent');
 
 module.exports = function (e) {
+    $(e.target).append('<i class="fa fa-circle-o-notch fa-spin"></i>');
     if (typeof fetch != 'function') {
         request
             .get($(e.target).data('link'))
         .end(function(err, res){
+            $(e.target).find('i').remove();
             if(err !== null){
                 console.error(err);
                 $('body').trigger('request-error');
@@ -16,9 +18,11 @@ module.exports = function (e) {
     } else {
         fetch($(e.target).data('link'))
             .then(function () {
+                $(e.target).find('i').remove();
                 $('body').trigger('request-success');
             })
             .catch(function (err) {
+                $(e.target).find('i').remove();
                 console.error(err);
                 $('body').trigger('request-error');
             });
